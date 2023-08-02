32db94b9c47d641772d6109c5da0ac3fcff03843
TarunKumar38 <tarunkumar3838@gmail.com>
2023-08-02 12:15:28 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CardComp from "./components/CardComp";
+import NewVideo from "./components/NewVideo";
+import Editor from "./components/Editor";
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/' exact>
+            <CardComp />
+          </Route>
+          <Route path='/:id' exact>
+            <div className='all-content video-editor'>
+              <div className='main-column1 editorr'>
+                <Editor />
+              </div>
+              <div className='main-column2 newvideo'>
+                <NewVideo />
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
